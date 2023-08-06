4c264ada97d45f823492d16382ae2d63d33e616b
Manicka Raj <96897008+RbY-Rj@users.noreply.github.com>
2023-08-06 12:18:10 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -735,7 +735,7 @@ async def advantage_spell_chok(msg):
     g_s += await search_gagala(msg.text)
     gs_parsed = []
     if not g_s:
-        k = await msg.reply("I couldn't find any movie in that name.")
+        k = await msg.reply("<b>⚠️Sorry !!! This Movie Is Not Available !\n\n Please Msg To ...\n\n ✌️Admin  ➠  @RbyAdminBot ⚡</b>.")
         await asyncio.sleep(8)
         await k.delete()
         return
